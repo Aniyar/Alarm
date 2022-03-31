507d9d283e4d5b60770aac43cc9f28d6bcb7ffc1
Aniyar <durmagambetovaa4@gmail.com>
2022-03-31 11:59:37 +0600

    video without modal

--- AlarmPP.Web/Components/Diagram/Video.razor.cs
+++ AlarmPP.Web/Components/Diagram/Video.razor.cs
@@ -35,7 +35,7 @@ namespace AlarmPP.Web.Components.Diagram
         public string Base64 { get; set; }
         public List<long> FileIdList { get; set; }
         public int Number { get; set; }
-        public int N_rows { get; set; } = 2;
+        public int N_rows { get; set; } 
         public bool ObjectsDialog { get; set; } = false;
 
         List<Gap> Gaps { get; set; } = new List<Gap>();
@@ -75,13 +75,22 @@ namespace AlarmPP.Web.Components.Diagram
                 var commonBitMap = new Bitmap(W * 5 - 87, H * N_rows - 175);
                 Graphics g = Graphics.FromImage(commonBitMap);
 
+                //for (int i = 0; i < N_rows; i++)
+                //{
+                //    g.DrawImageUnscaled(rows[N_rows - i - 1][0], 0, (H - upperKoef) * i);
+                //    g.DrawImageUnscaled(rows[N_rows - i - 1][1], W, (H - upperKoef) * i );
+                //    g.DrawImageUnscaled(rows[N_rows - i - 1][2], W * 2, (H - upperKoef) * i);
+                //    g.DrawImageUnscaled(rows[N_rows - i - 1][3], W * 3, (H - upperKoef) * i);
+                //    g.DrawImageUnscaled(rows[N_rows - i - 1][4], W * 4, (H - upperKoef) * i);
+                //}
+
                 for (int i = 0; i < N_rows; i++)
                 {
-                    g.DrawImageUnscaled(RotateImage(rows[N_rows-i-1][0], -1), -12, (H - upperKoef) * i - 46);
-                    g.DrawImageUnscaled(RotateImage(rows[N_rows-i-1][1], 1), W - 12, (H - upperKoef) * i - 65);
-                    g.DrawImageUnscaled(RotateImage(rows[N_rows-i-1][2], 1), W * 2 - 33, (H - upperKoef) * i - 35);
-                    g.DrawImageUnscaled(RotateImage(rows[N_rows-i-1][3], -3), W * 3 - 50, (H - upperKoef) * i - 24);
-                    g.DrawImageUnscaled(RotateImage(rows[N_rows-i-1][4], 4), W * 4 - 130, (H - upperKoef) * i - 24);
+                    g.DrawImageUnscaled(RotateImage(rows[N_rows - i - 1][0], -1), 0, (H - upperKoef) * i - 46);
+                    g.DrawImageUnscaled(RotateImage(rows[N_rows - i - 1][1], 1), W, (H - upperKoef) * i - 65);
+                    g.DrawImageUnscaled(RotateImage(rows[N_rows - i - 1][2], 1), W * 2, (H - upperKoef) * i - 35);
+                    g.DrawImageUnscaled(RotateImage(rows[N_rows - i - 1][3], -3), W * 3, (H - upperKoef) * i - 24);
+                    g.DrawImageUnscaled(RotateImage(rows[N_rows - i - 1][4], 4), W * 4, (H - upperKoef) * i - 24);
                 }
 
                 if (rows[1] != null)
