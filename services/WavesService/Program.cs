5a85a51722509524e4d338abc84ef8c7a2fb2209
Aniyar <durmagambetovaa4@gmail.com>
2022-05-16 17:01:20 +0600

    раздельные сервисы

--- services/WavesService/Program.cs
+++ services/WavesService/Program.cs
@@ -1,17 +1,14 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProfilPoverhService
+namespace WavesService
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -19,15 +16,9 @@ namespace ProfilPoverhService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseWindowsService()
-                .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
-                    IConfiguration configuration = hostContext.Configuration;
-                    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(hostContext.Configuration).CreateLogger();
-                    services.Configure<RabbitMQConfiguration>(configuration.GetSection(nameof(RabbitMQConfiguration)));
                     services.AddHostedService<Worker>();
                 });
-        
     }
 }
